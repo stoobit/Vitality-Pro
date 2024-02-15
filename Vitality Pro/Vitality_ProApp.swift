0bb780ba06d65decc994fefec7dead4383f72184
stoobit <till.bruegmann@icloud.com>
2024-02-15 17:13:36 +0100

    Initial Commit

---
+++ Vitality Pro/Vitality_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vitality_ProApp.swift
+//  Vitality Pro
+//
+//  Created by Till Brügmann on 15.02.24.
+//
+
+import SwiftUI
+
+@main
+struct Vitality_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
